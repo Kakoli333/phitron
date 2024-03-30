3dbbeadd242041a6c5e82aa42acf58f3bc0767fe
Kakoli333 <163674387+Kakoli333@users.noreply.github.com>
2024-03-30 22:29:43 +0600

    Add files via upload

---
+++ week 3/day 1/day 2/double_strings.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define yes count<<"Yes"
+#define no cout<<"No"
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+       ll n;
+       cin>>n;
+    }
+}
+
+
+
+
